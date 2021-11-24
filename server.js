6b8544c43f90f595fb8fbffdf532e0d18cb590d7
nskidmore7 <njskid11@gmail.com>
2021-11-23 22:08:44 -0600

    Bringing over code provided

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const { clog } = require('./middleware/clog');
+const api = require('./routes/index.js');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use("/api", api);
+app.use(express.static("public"));
+
