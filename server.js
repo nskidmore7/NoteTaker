686ac26baef5d466da2e9f7606e523d383485b55
nskidmore7 <njskid11@gmail.com>
2021-11-26 17:41:41 -0600

    get/post

--- server.js
+++ server.js
@@ -12,3 +12,12 @@ app.use(express.urlencoded({extended: true}));
 app.use("/api", api);
 app.use(express.static("public"));
 
+app.get("*", (req, res) => 
+    res.sendFile(path.join(__dirname, "./public/index.html")));
+
+    app.get("/notes", (req, res) => 
+    res.sendFile(path.join(__dirname, "./public/notes.html")));
+
+    app.listen(PORT, () =>
+    console.log(`Listening at http://localhost:${PORT}`));
+
